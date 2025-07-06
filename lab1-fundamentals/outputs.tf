1c49c0e8d377e649b163da2854d07a39391c9197
R4Z0R <accounts.sm@protonmail.com>
2025-07-06 15:14:11 +1000

    Seneitive Inputs and Outputs module updates made.

--- lab1-fundamentals/outputs.tf
+++ lab1-fundamentals/outputs.tf
@@ -2,14 +2,19 @@ output "application_name" {
     value = var.application_name
 }
 
-output environment_name{
+output "environment_name"{
     value = var.environment_name
 }
 
-output environment_prefix {
+output "environment_prefix"{
     value = local.environment_prefix
 }
 
-output suffix{
+output "suffix"{
     value = random_string.suffix.result
+}
+
+output "api_key" {
+    value = var.api_key
+    sensitive = true # This will ensure the API key is not displayed in the output
 }
--- lab1-fundamentals/variables.tf
+++ lab1-fundamentals/variables.tf
@@ -11,3 +11,7 @@ variable "environment_name" {
 }
 
 # terraform plan -var "application_name=samsblog" -var "environment_name=dev"
+
+variable api_key{
+  sensitive = true # value will not be displayed in the plan or apply output
+} 

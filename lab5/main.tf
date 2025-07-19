f2b35bd6f733d1ed873810371700a0afa66361b5
R4Z0R <accounts.sm@protonmail.com>
2025-07-19 19:58:28 +1000

    Added lab 5 subnet code

--- lab5/main.tf
+++ lab5/main.tf
@@ -1,4 +1,52 @@
+
 resource "azurerm_resource_group" "main"  {
     name = "rg-${var.application_name}-${var.environment_name}"
     location = var.primary_location
+}
+
+// /22 1024 IP addresses
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = [var.base_address_space]
+}
+
+locals {
+    alpha_address_space     = cidrsubnet(var.base_address_space, 2, 0) 
+    bravo_address_space     = cidrsubnet(var.base_address_space, 2, 1)
+    charlie_address_space   = cidrsubnet(var.base_address_space, 2, 2)
+    delta_address_space     = cidrsubnet(var.base_address_space, 2, 3)
+}
+
+//10.39.0.0/24
+resource "azurerm_subnet" "alpha" {
+  name                 = "snet-alpha"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.alpha_address_space]
+}
+
+//10.39.1.0/24
+resource "azurerm_subnet" "bravo" {
+  name                 = "snet-bravo"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.bravo_address_space]
+}
+
+//10.39.2.0/24
+resource "azurerm_subnet" "charlie" {
+  name                 = "snet-charlie"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.charlie_address_space]
+}
+
+//10.39.3.0/24
+resource "azurerm_subnet" "delta" {
+  name                 = "snet-delta"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.delta_address_space]
 }
--- lab5/variables.tf
+++ lab5/variables.tf
@@ -9,3 +9,7 @@ variable "environment_name" {
 variable "primary_location" {
   type        = string
 }
+
+variable "base_address_space" {
+  type        = string
+}
